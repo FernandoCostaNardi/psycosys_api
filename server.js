37b7d7c5f1f72bd6b7974992be687d50265fc0e8
Fernando <fc-nardi@hotmail.com>
2019-07-18 21:51:35 -0300

    add routes

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
+const createTables = require("./src/config/dbTables");
 
 const app = express();
-app.use(cors());
 app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
 
-// app.use('api/', require('./src/./src/routes.js'));
+app.use("/api", require("./src/routes"));
 
-app.listen(3002);
+app.listen(3009);
